491406c3ed2f2f7a97993db59670a2504bd9b8ed
HeavyPunk <kirillkuchma8@gmail.com>
2024-01-25 15:29:31 +0500

    Changed settings

--- .ci/docker-prod.sh
+++ .ci/docker-prod.sh
@@ -3,6 +3,6 @@ set -eu
 bash build.sh
 
 DOCKER_IMAGE=kirieshki/simple-hosting.compositor
-DOCKER_TAG=latest
+DOCKER_TAG=2024.01.17-amd64
 
-docker buildx build -t $DOCKER_IMAGE:$DOCKER_TAG --progress plain --platform linux/arm64/v8 --push .
+docker buildx build -t $DOCKER_IMAGE:$DOCKER_TAG --progress plain --platform linux/amd64 --push .
